eda4b050f90c9b025f0ebbba511edb04a8377fe4
sebathefox <seba4928@edu.sde.dk>
2018-11-12 19:21:53 +0100

    Initial Commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    private Collider2D coll;
+
+    public float speed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+
+        coll.
+
+        float js = Input.GetAxis("Jump") * speed;
+
+        translation *= Time.fixedDeltaTime;
+
+        js *= Time.fixedDeltaTime;
+
+        transform.Translate(translation, js, 0);
+        
+    }
+}
---
+++ Assets/Scripts/World/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.World
+{
+    public class Generator : MonoBehaviour
+    {
+        public Transform Block;
+
+        void Start()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Instantiate(Block, new Vector3(i, 0, 0), Quaternion.identity);
+            }
+        }
+    }
+}
